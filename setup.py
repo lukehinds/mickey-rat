7d465ade846c441e3edc7b739ee8e0b33b40a108
Luke Hinds <luke@stacklok.com>
2023-10-20 11:09:12 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mickey_rat",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies your package needs
+    ],
+    author="Mickey Rat",
+    author_email="mickeyrat@gmail.com",
+    description="Don't use this package.",
+    long_description="Don't use this package.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/torvalds/linux", # If you have a repo for your package
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
